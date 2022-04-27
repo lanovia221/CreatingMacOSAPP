201d55ceb5804d64417dd34cc4dd8e6614d7fabe
Lanovia221 <lanovialiem01@student.ciputra.ac.id>
2022-04-27 09:23:01 +0700

    Initial Commit

---
+++ MacLandmarks/ContentView.swift
@@ -0,0 +1,18 @@
+
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        LandmarkList()
+            .frame(minWidth: 700, minHeight: 300)
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+            .environmentObject(ModelData())
+            .frame(width: 850, height: 700)
+    }
+}
